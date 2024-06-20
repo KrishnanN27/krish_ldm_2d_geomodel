aee2cfc478cf7c70c4952c1f0160916aa5b4ec1c
guidodf09 <163361716+guidodf09@users.noreply.github.com>
2024-06-20 09:13:05 -0700

    Update prepare_dataset.py

--- scripts/prepare_dataset.py
+++ scripts/prepare_dataset.py
@@ -2,7 +2,7 @@
 '''
 File: prepare_dataset.py
 Author: Guido Di Federico
-Description: Used for loading a diffusers "dataset"-like dataset of geomodels and save it as .jpg images
+Description: Used for loading a diffusers "dataset"-like dataset of geomodels and save it as .jpg images in a folder
 Note: requires Python package "datasets" by HuggingFace to open dataset
 '''
 
@@ -12,19 +12,19 @@ import numpy as np
 import cv2
 from datasets import Dataset
 
-# Set directories for diffusers dataset and .jpg images folder
-dataset_dir       = './data/diffusers_dataset/'
-imgs_dir          = './imgs/'
+# Set directories for dataset and .jpg images folder
+dataset_dir = './data/diffusers_dataset/'
+imgs_dir = './imgs/'
 
 if not os.path.exists(imgs_dir):
     os.makedirs(imgs_dir)
     
 # Load diffusers dataset and transform into NumPy
 diffusers_dataset = Dataset.load_from_disk(dataset_dir)['image']
-geomodels_numpy_rot   = np.array([np.array(image) for image in diffusers_dataset]) 
-geomodels_numpy   = np.rot90(np.flip(geomodels_numpy_rot, axis=2), k=-1, axes=(1, 2))
+geomodels_numpy_rot = np.array([np.array(image) for image in diffusers_dataset]) 
+geomodels_numpy = np.rot90(np.flip(geomodels_numpy_rot, axis=2), k=-1, axes=(1, 2))
 
-# Transform every NumPy geomodel into .jpg image and save
+# Transform every NumPy geomodel into .jpg image and save in folder
 for i in range(len(diffusers_dataset)):
     img_path = os.path.join(imgs_dir, f"image_{i}.jpeg")
     cv2.imwrite(img_path, geomodels_numpy[i,:,:])
