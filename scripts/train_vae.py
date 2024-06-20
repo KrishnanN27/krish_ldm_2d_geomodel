370d0ffc2f4125367d12f0fa0d262880f130d6d4
guidodf09 <163361716+guidodf09@users.noreply.github.com>
2024-06-20 11:25:56 -0700

    Update train_vae.py

--- scripts/train_vae.py
+++ scripts/train_vae.py
@@ -34,14 +34,21 @@ from generative.networks.schedulers import DDPMScheduler, DDIMScheduler
 
 # Set directories
 imgs_dir          =  '../data/imgs/'
+trained_vae_dir = './trained_vae/'
+
+if not os.path.exists(trained_vae_dir):
+    os.makedirs(trained_vae_dir)
+    
+# Choose device
+#device = torch.device("cpu")
+device = torch.device("cuda")
+
 
 
 # Load dataset
 geomodels_dataset = [{"image": imgs_dir + img} for  img in os.listdir(imgs_dir)][:4000]
 N_data            = len(geomodels_dataset)
 image_size        = 64
-device = torch.device("cpu")
-device = torch.device("cuda")
 
 
 # Split dataset
@@ -50,9 +57,9 @@ val_split         = 0.2
 test_split        = 1 - train_split - val_split
 batch_size        = 16
 
-train_datalist    = geomodels_dataset[:int(N_data*train_split)]
-val_datalist      = geomodels_dataset[int(len(train_datalist)):int(N_data*(1-test_split))+1]
-test_datalist     = geomodels_dataset[int(-N_data*test_split):]
+m_train_list    = geomodels_dataset[:int(N_data*train_split)]
+m_val_list      = geomodels_dataset[int(len(m_train_list)):int(N_data*(1-test_split))+1]
+m_test_list     = geomodels_dataset[int(-N_data*test_split):]
 
 # Transform dataset
 
@@ -64,8 +71,8 @@ train_transforms = transforms.Compose(
         transforms.ScaleIntensityRanged(keys=["image"], a_min=0.0, a_max=255.0, b_min=0.0, b_max=1.0, clip=True)]
 )
 
-train_ds = Dataset(data=train_datalist, transform=train_transforms)
-train_loader = DataLoader(train_ds, batch_size=batch_size, shuffle=True)
+m_train_ds = Dataset(data=m_train_list, transform=train_transforms)
+m_train_loader = DataLoader(m_train_ds, batch_size=batch_size, shuffle=True)
 
 # Validation set
 val_transforms = transforms.Compose(
@@ -75,8 +82,8 @@ val_transforms = transforms.Compose(
         transforms.ScaleIntensityRanged(keys=["image"], a_min=0.0, a_max=255.0, b_min=0.0, b_max=1.0, clip=True),
     ]
 )
-val_ds = Dataset(data=val_datalist, transform=val_transforms)
-val_loader = DataLoader(val_ds, batch_size=batch_size, shuffle=True)
+m_val_ds = Dataset(data=m_val_list, transform=val_transforms)
+m_val_loader = DataLoader(m_val_ds, batch_size=batch_size, shuffle=True)
 
 # Testing set
 test_transforms = transforms.Compose(
@@ -87,8 +94,8 @@ test_transforms = transforms.Compose(
     ]
 )
 
-test_ds = Dataset(data=test_datalist, transform=val_transforms)
-test_loader = DataLoader(test_ds, batch_size=batch_size, shuffle=True)
+m_test_ds = Dataset(data=m_test_list, transform=val_transforms)
+m_test_loader = DataLoader(m_test_ds, batch_size=batch_size, shuffle=True)
 
 # Set hard data conditioning points (first two coordinates are (x,y) points and third coordinate the pixel value)
 hard_data_locations = np.array([[7,7, 255], 
@@ -111,99 +118,107 @@ autoencoderkl = AutoencoderKL(
 autoencoderkl = autoencoderkl.to(device)
 
 # Train the VAE on three loss terms: (1) reconstruction loss, (2) K-L divergence loss, (3) hard data facies loss
-trained_vae_dir = './trained_vae/'
-if not os.path.exists(trained_vae_dir):
-    os.makedirs(trained_vae_dir)
-    
-
 
 # Training parameters
-n_epochs     = 100
-val_interval = 10
-autoencoder_warm_up_n_epochs = 10
-epoch_losses = []
-val_losses   = []
-epoch_recon_losses = []
-epoch_gen_losses = []
-epoch_disc_losses = []
-val_recon_losses = []
-intermediary_images = []
-num_example_images = 4
-kl_weight = 1e-6
-lambda_hd = 0
+n_epochs      = 100
+val_interval  = 10
+save_interval = 1
+kl_weight     = 1e-6
+hd_weight     = 1e1
+
 
 # Gradient parameters (optimizer and scaler)
-optimizer_g = torch.optim.Adam(autoencoderkl.parameters(), lr=1e-4)
-scaler_g = torch.cuda.amp.GradScaler()
-scaler = GradScaler()
+optimizer = torch.optim.Adam(autoencoderkl.parameters(), lr=1e-4)
+scaler = torch.cuda.amp.GradScaler()
 
 # Training loop
+
+epoch_losses = []
+val_losses   = []
 
 for epoch in range(n_epochs):
         
     autoencoderkl.train()
+    epoch_recon_loss = 0
+    epoch_kl_loss = 0
+    epoch_hd_loss = 0
     epoch_loss = 0
-    gen_epoch_loss = 0
-    progress_bar = tqdm(enumerate(train_loader), total=len(train_loader), ncols=110)
+    progress_bar = tqdm(enumerate(m_train_loader), total=len(m_train_loader), ncols=110)
     progress_bar.set_description(f"Epoch {epoch}")
 
     for step, batch in progress_bar:
-        images = batch["image"].to(device)
-        optimizer_g.zero_grad(set_to_none=True)
+        m_batch = batch["image"].to(device)
+        optimizer.zero_grad(set_to_none=True)
 
         with autocast(enabled=True):
-            reconstruction, z_mu, z_sigma = autoencoderkl(images)
+            
+            reconstruction, z_mu, z_sigma = autoencoderkl(m_batch)
+            recons_loss = F.l1_loss(reconstruction.float(), m_batch.float())
+
             reconstruction_hd = [reconstruction[...,loc[0],loc[1]] for loc in hard_data_locations]
             reconstruction_hd_vector =  torch.stack(reconstruction_hd, dim=0).flatten()
-            images_hd = [images[...,loc[0],loc[1]] for loc in hard_data_locations]
-            images_hd_vector = torch.stack(images_hd, dim=0).flatten()
-            hd_loss =  F.mse_loss(images_hd_vector * lambda_hd, reconstruction_hd_vector * lambda_hd)
+            m_batch_hd = [m_batch[...,loc[0],loc[1]] for loc in hard_data_locations]
+            m_batch_hd_vector = torch.stack(m_batch_hd, dim=0).flatten()
+            hd_loss =  F.mse_loss(m_batch_hd_vector, reconstruction_hd_vector)
 
-            recons_loss = F.l1_loss(reconstruction.float(), images.float())
 
             kl_loss = 0.5 * torch.sum(z_mu.pow(2) + z_sigma.pow(2) - torch.log(z_sigma.pow(2)) - 1, dim=[1, 2, 3])
             kl_loss = torch.sum(kl_loss) / kl_loss.shape[0]
-            loss_g = recons_loss + (kl_weight * kl_loss) + hd_loss
+            
+            loss_tot = recons_loss + (kl_weight * kl_loss) + (hd_weight * hd_loss)
 
 
-        scaler_g.scale(loss_g).backward()
-        scaler_g.step(optimizer_g)
-        scaler_g.update()
+        scaler.scale(loss_tot).backward()
+        scaler.step(optimizer)
+        scaler.update()
 
-        epoch_loss += recons_loss.item()
-        if epoch > autoencoder_warm_up_n_epochs:
-            gen_epoch_loss += generator_loss.item()
+        epoch_recon_loss += recons_loss.item()
+        epoch_kl_loss += kl_loss.item() * kl_weight
+        epoch_hd_loss += hd_loss.item() * hd_weight
+        
+        epoch_loss += loss_tot.item()
 
         progress_bar.set_postfix(
             {
-                "recons_loss": epoch_loss / (step + 1),
-                "kl_loss": kl_loss.item() / (step + 1),
-                "hd_loss": hd_loss.item() / (step + 1),
+                "recons_loss": epoch_recon_loss / (step + 1),
+                "kl_loss": epoch_kl_loss / (step + 1),
+                "hd_loss": epoch_hd_loss / (step + 1),
             }
         )
-    epoch_recon_losses.append(epoch_loss / (step + 1))
-    epoch_gen_losses.append(gen_epoch_loss / (step + 1))
-    if (epoch + 1) % 10 == 0:
-        torch.save(autoencoderkl.state_dict(), f'{trained_vae_dir} + /vae_epoch_{epoch + 1}.pt')
+    
+    epoch_losses.append(epoch_loss / (step + 1))
+    
+    if (epoch + 1) % save_interval == 0:
+        torch.save(autoencoderkl.state_dict(), f'{trained_vae_dir}' + f'/vae_epoch_{epoch + 1}.pt')
 
     if (epoch + 1) % val_interval == 0:
         autoencoderkl.eval()
         val_loss = 0
         with torch.no_grad():
-            for val_step, batch in enumerate(val_loader, start=1):
-                images = batch["image"].to(device)
+            for val_step, batch in enumerate(m_val_loader, start=1):
+                m_batch = batch["image"].to(device)
 
                 with autocast(enabled=True):
-                    reconstruction, z_mu, z_sigma = autoencoderkl(images)
-                    if val_step == 1:
-                        intermediary_images.append(reconstruction[:num_example_images, 0])
+                    reconstruction, z_mu, z_sigma = autoencoderkl(m_batch)
+                    recons_loss = F.l1_loss(reconstruction.float(), m_batch.float())
 
-                    recons_loss = F.l1_loss(images.float(), reconstruction.float())
+                    reconstruction_hd = [reconstruction[...,loc[0],loc[1]] for loc in hard_data_locations]
+                    reconstruction_hd_vector =  torch.stack(reconstruction_hd, dim=0).flatten()
+                    m_batch_hd = [m_batch[...,loc[0],loc[1]] for loc in hard_data_locations]
+                    m_batch_hd_vector = torch.stack(m_batch_hd, dim=0).flatten()
+                    hd_loss =  F.mse_loss(m_batch_hd_vector, reconstruction_hd_vector)
 
-                val_loss += recons_loss.item()
+
+                    kl_loss = 0.5 * torch.sum(z_mu.pow(2) + z_sigma.pow(2) - torch.log(z_sigma.pow(2)) - 1, dim=[1, 2, 3])
+                    kl_loss = torch.sum(kl_loss) / kl_loss.shape[0]
+
+                    loss_g = recons_loss + (kl_weight * kl_loss) + (hd_weight * hd_loss)
+
+
+                val_loss += loss_g.item()
 
         val_loss /= val_step
-        val_recon_losses.append(val_loss)
+        val_losses.append(val_loss)
         print(f"epoch {epoch + 1} val loss: {val_loss:.4f}")
+        
 progress_bar.close()
-
